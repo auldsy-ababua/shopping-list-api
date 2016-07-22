5f0eb292e08a41264723caef61a774d32a659b24
Colin Aulds <caulds989@gmail.com>
2016-07-21 17:41:31 -0700

    fixed put test, now working on delete

--- test/test-server.js
+++ test/test-server.js
@@ -88,5 +88,28 @@ describe('Shopping List', function() {
               done();
             });
     });
-    it('should delete an item on DELETE');
+    it('should delete an item on DELETE', function(done){
+        chai.request(app)
+            .delete('/items/0')
+            .end(function(err, res) {
+              should.equal(err, null);
+              res.should.have.status(200);
+              res.should.be.json;
+              res.body.should.be.a('object');
+              res.body.should.have.property('name');
+              res.body.should.have.property('id');
+              res.body.name.should.be.a('string');
+              res.body.id.should.be.a('number');
+              res.body.name.should.equal('Colin');
+              storage.items.should.be.a('array');
+              storage.items.should.have.length(4);
+              storage.items[0].should.be.a('object');
+              storage.items[0].should.have.property('id');
+              storage.items[0].should.have.property('name');
+              storage.items[0].id.should.be.a('number');
+              storage.items[0].name.should.be.a('string');
+              storage.items[0].name.should.equal('Colin');
+              done();
+            });
+    });
 });
